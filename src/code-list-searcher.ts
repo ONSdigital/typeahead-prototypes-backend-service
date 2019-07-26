78b0da091ae0a7cc1e5819533fffe21b974a190e
Ben Meyrick <bameyrick@gmail.com>
2019-07-26 13:11:28 +0100

    Update to allow GETs

--- src/code-list-searcher.ts
+++ src/code-list-searcher.ts
@@ -1,4 +1,4 @@
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
 import * as Fuse from 'fuse.js';
 import  { FuseOptions } from 'fuse.js';
 
@@ -41,52 +41,77 @@ export default function initialiseSearch(server: Express): void {
     if (codeLists.hasOwnProperty(listName)) {
       const list: ICodeListItem[] = (<any>codeLists)[listName];
 
-      server.post(`/${listName}`, (request, response) => {
-        const origin = <string>request.headers.origin;
+      server.get(`/${listName}`, (request, response) => handlePost(request, response, list));
 
-        if (originIsAllowed(origin)) {
-          const query = (request.body.query || '').trim().toLowerCase();
-          const language = request.body.lang;
-          const maxResults = request.body.max ? parseInt(request.body.max, 10) : 10;
-          const languages: string[] = [];
-
-          if (language) {
-            languages.push(language.trim().toLowerCase());
-          }
-
-          if (!languages.includes('en-gb')) {
-            languages.push('en-gb');
-          }
-
-          const fuse = new Fuse(list,  <FuseOptions<any>>{
-            shouldSort: true,
-            caseSensitive: false,
-            keys: languages,
-            threshold: 0.2
-          });
-          
-          const allResults = fuse.search(query);
-          const results: any[] = allResults.slice(0, maxResults);
-          
-          if (language && language === 'en') {
-            results.forEach(result => result['en'] = result['en-gb']);
-          }
-
-          const responseData = {
-            results: results,
-            totalResults: allResults.length
-          };
-
-          response.setHeader('Access-Control-Allow-Origin', origin);
-
-          response.send(responseData);
-        } else {
-          response.status(401);
-          response.send();
-        }
-      });
+      // Used in prototypes - GET should be used otherwise
+      server.post(`/${listName}`, (request, response) => handleGet(request, response, list));
     }
   }
+}
+
+function handleGet(request: Request, response: Response, list: ICodeListItem[]) {
+  const origin = <string>request.headers.origin;
+
+  if (originIsAllowed(origin)) {
+    const query = request.query.query;
+    const language = request.query.lang;
+    const maxResults = request.query.max ? parseInt(request.query.max, 10) : 10;
+
+    handleRequest(request, response, origin, list, query, language, maxResults);
+  } else {
+    response.status(401);
+    response.send();
+  }
+}
+
+function handlePost(request: Request, response: Response, list: ICodeListItem[]) {
+  const origin = <string>request.headers.origin;
+
+  if (originIsAllowed(origin)) {
+    const query = (request.body.query || '').trim().toLowerCase();
+    const language = request.body.lang;
+    const maxResults = request.body.max ? parseInt(request.body.max, 10) : 10;
+    
+    handleRequest(request, response, origin, list, query, language, maxResults);
+  } else {
+    response.status(401);
+    response.send();
+  }
+}
+
+function handleRequest(request: Request, response: Response, origin: string, list: ICodeListItem[], query: string, language: string, maxResults: number) {
+  const languages: string[] = [];
+
+  if (language) {
+    languages.push(language.trim().toLowerCase());
+  }
+
+  if (!languages.includes('en-gb')) {
+    languages.push('en-gb');
+  }
+
+  const fuse = new Fuse(list,  <FuseOptions<any>>{
+    shouldSort: true,
+    caseSensitive: false,
+    keys: languages,
+    threshold: 0.2
+  });
+  
+  const allResults = fuse.search(query);
+  const results: any[] = allResults.slice(0, maxResults);
+  
+  if (language && language === 'en') {
+    results.forEach(result => result['en'] = result['en-gb']);
+  }
+
+  const responseData = {
+    results: results,
+    totalResults: allResults.length
+  };
+
+  response.setHeader('Access-Control-Allow-Origin', origin);
+
+  response.send(responseData);
 }
 
 function originIsAllowed(origin: string) {
